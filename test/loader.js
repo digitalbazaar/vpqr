3530b3f239107d9d0542d68ae137670fb5412970
Dave Longley <dlongley@digitalbazaar.com>
2023-11-29 21:19:29 -0500

    Update did-key-related dev deps.

--- test/loader.js
+++ test/loader.js
@@ -1,19 +1,17 @@
 /*!
  * Copyright (c) 2021-2023 Digital Bazaar, Inc. All rights reserved.
  */
-import * as didKey from '@digitalbazaar/did-method-key';
 import ageContext from 'age-verification-context';
 import {CachedResolver} from '@digitalbazaar/did-io';
 import citContext from 'cit-context';
 import cred from 'credentials-context';
 import didContext from 'did-context';
+import {driver} from '@digitalbazaar/did-method-key';
 import ed25519 from 'ed25519-signature-2020-context';
+import {Ed25519VerificationKey2020} from
+  '@digitalbazaar/ed25519-verification-key-2020';
 import {JsonLdDocumentLoader} from 'jsonld-document-loader';
 import x25519 from 'x25519-key-agreement-2020-context';
-
-const didKeyDriver = didKey.driver();
-const resolver = new CachedResolver();
-resolver.use(didKeyDriver);
 
 const {contexts: credentialsContext, constants: {CREDENTIALS_CONTEXT_V1_URL}} =
   cred;
@@ -36,7 +34,13 @@ staticLoader.addStatic(didContext.constants.DID_CONTEXT_URL,
 staticLoader.addStatic(CREDENTIALS_CONTEXT_V1_URL,
   credentialsContext.get(CREDENTIALS_CONTEXT_V1_URL));
 
+const resolver = new CachedResolver();
+const didKeyDriver = driver();
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'z6Mk',
+  fromMultibase: Ed25519VerificationKey2020.from
+});
+resolver.use(didKeyDriver);
 staticLoader.setDidResolver(resolver);
 
 export const documentLoader = staticLoader.documentLoader.bind(staticLoader);
-
