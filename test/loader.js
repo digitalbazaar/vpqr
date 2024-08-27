fa40d327a8f55047d81918481ca5b4370c1c7c8e
David I. Lehn <dlehn@digitalbazaar.com>
2024-08-26 20:59:02 -0400

    Update test dependencies.
    - Update dependencies.
    - Update loader setup to use `addDocuments`.

--- test/loader.js
+++ test/loader.js
@@ -1,38 +1,31 @@
 /*!
- * Copyright (c) 2021-2023 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Digital Bazaar, Inc. All rights reserved.
  */
-import ageContext from 'age-verification-context';
-import {CachedResolver} from '@digitalbazaar/did-io';
+import {contexts as ageContexts} from '@digitalbazaar/age-verification-context';
+import {contexts as didContexts} from 'did-context';
+import {contexts as ed25519Contexts} from 'ed25519-signature-2020-context';
+import {contexts as vcContexts} from 'credentials-context';
+import {contexts as x25519Contexts} from 'x25519-key-agreement-2020-context';
+
 import citContext from 'cit-context';
-import cred from 'credentials-context';
-import didContext from 'did-context';
+const {contexts: citContexts} = citContext;
+
+import {CachedResolver} from '@digitalbazaar/did-io';
 import {driver} from '@digitalbazaar/did-method-key';
-import ed25519 from 'ed25519-signature-2020-context';
 import {Ed25519VerificationKey2020} from
   '@digitalbazaar/ed25519-verification-key-2020';
 import {JsonLdDocumentLoader} from 'jsonld-document-loader';
-import x25519 from 'x25519-key-agreement-2020-context';
-
-const {contexts: credentialsContext, constants: {CREDENTIALS_CONTEXT_V1_URL}} =
-  cred;
 
 const staticLoader = new JsonLdDocumentLoader();
-staticLoader.addStatic(ed25519.constants.CONTEXT_URL,
-  ed25519.contexts.get(ed25519.constants.CONTEXT_URL));
 
-staticLoader.addStatic(x25519.constants.CONTEXT_URL,
-  x25519.contexts.get(x25519.constants.CONTEXT_URL));
-
-staticLoader.addStatic(citContext.constants.CONTEXT_URL,
-  citContext.contexts.get(citContext.constants.CONTEXT_URL));
-
-staticLoader.addStatic(ageContext.CONTEXT_URL_V1, ageContext.CONTEXT_V1);
-
-staticLoader.addStatic(didContext.constants.DID_CONTEXT_URL,
-  didContext.contexts.get(didContext.constants.DID_CONTEXT_URL));
-
-staticLoader.addStatic(CREDENTIALS_CONTEXT_V1_URL,
-  credentialsContext.get(CREDENTIALS_CONTEXT_V1_URL));
+staticLoader.addDocuments({documents: [
+  ...ageContexts,
+  ...citContexts,
+  ...didContexts,
+  ...ed25519Contexts,
+  ...vcContexts,
+  ...x25519Contexts
+]});
 
 const resolver = new CachedResolver();
 const didKeyDriver = driver();
